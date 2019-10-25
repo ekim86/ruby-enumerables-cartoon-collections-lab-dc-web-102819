c77e62ea346117fc5b4e25b56f076769ac7236d3
eunice kim <oonis86@gmail.com>
2019-10-25 13:42:43 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,7 +2,7 @@ dwarves = ["Doc", "Dopey", "Bashful", "Grumpy"]
 planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
 call_screams = ["bo", "bam", "boom", "bazam", "powzer"]
 
-def roll_call_dwarves(array)
+def roll_call_dwarves(dwarves)
   i = 0 
   while i < array.length 
     puts "#{i + 1}. #{array[i]}"
