0a4370eb994c01815bf3d400df022559c17d9505
eunice kim <oonis86@gmail.com>
2019-10-25 13:44:56 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,3 +10,44 @@ def roll_call_dwarves(dwarves)
   end 
 end
 
+def summon_captain_planet(veggies)
+  nu_veggies = []
+  i = 0 
+  while i < veggies.length 
+    nu_veggies << veggies[i].capitalize + "!"
+    i += 1 
+  end 
+  nu_veggies
+end
+
+def long_planeteer_calls(fruits) 
+  i = 0 
+  if  fruits.any? {|i| i.length > 4}
+    return true
+  else 
+    return false
+  i = i + 1
+  end 
+end
+
+cheese_types = ["cheddar", "gouda", "camembert"]
+snacks = ["crackers", "gouda", "thyme"]
+
+def find_the_cheese(array)
+  cheese_types = ["cheddar", "gouda", "camembert"]
+    array.find do |type|
+    cheese_types.include?(type)
+  end 
+end
+
+cheese_types = ["cheddar", "gouda", "camembert"]
+snacks = ["crackers", "gouda", "thyme"]
+
+def find_the_cheese2(array, array2)
+  cheese_types = array2
+    array.find do |type|
+    cheese_types.include?(type)
+  end 
+end
+
+
